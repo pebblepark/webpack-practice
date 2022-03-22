54883c77038c302ea6a0f380b3edfefc0c1663ee
pebblepark <subling02@gmail.com>
2022-03-22 15:57:31 +0900

    feat: style-loader, css-loader 적용 예제 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _ from "lodash";
+import "./common.css";
 
 function component() {
   var element = document.createElement("div");
+  element.classList.add("text");
 
   /* lodash is required for the next line to work */
   element.innerHTML = _.join(["Hello", "webpack"], " ");
